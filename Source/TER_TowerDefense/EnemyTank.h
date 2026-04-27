e23276cd44f8610131375d6fb4e93cb48d1a574a
Zyre2 <babies11@hotmail.fr>
2026-04-27 17:13:08 +0200

    missed

---
+++ Source/TER_TowerDefense/EnemyTank.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "EnemyTank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TER_TOWERDEFENSE_API AEnemyTank : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+	AEnemyTank();
+};
